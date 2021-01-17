4e37fb4fa8191fda38e5894a1ea8fca5f86b9f17
fukuroda <fukuroder@live.jp>
2021-01-17 23:08:58 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo docker run --gpus all --rm -v $PWD/wavenet:/wavenet -w /wavenet -it pytorch_lightning_wavenet:latest bash
+sudo docker run --shm-size=512m --gpus all --rm -v $PWD/wavenet:/wavenet -w /wavenet -it pytorch_lightning_wavenet:latest bash
